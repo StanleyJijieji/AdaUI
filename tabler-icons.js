5f025dc8d8f52ae45f87778d7b8094271360874e
Stanley <81696141+StanleyJijieji@users.noreply.github.com>
2025-12-23 01:56:44 +0800

    feat: 添加多个数据库和服务的SVG图标资源
    新增包括Tabler Icons、Netsuite、Facebook Ads、Amazon RDS、SQLite、Google Sheets、eBay、Google Drive、GitHub、API、MongoDB、Amazon S3、HubSpot、Notion、Shopify、MySQL、Stripe、QuickBooks和Azure SQL Database等服务的SVG图标文件

---
+++ tabler-icons.js
@@ -0,0 +1,10 @@
+/**
+ * Tabler Icons Webfont
+ * 
+ * No JavaScript is required for the webfont version of Tabler Icons.
+ * Simply include the 'tabler-icons.css' file in your HTML or CSS.
+ * 
+ * Usage:
+ * <i class="ti ti-brand-github"></i>
+ */
+console.log('Tabler Icons loaded (Webfont version)');
